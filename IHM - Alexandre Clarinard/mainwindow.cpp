3c690c4316535a4a289811083bff5628d18da855
Antoine MATTEI <antoine.mattei02@orange.fr>
2021-05-28 08:49:21 +0200

    Mise à jour

--- IHM - Alexandre Clarinard/mainwindow.cpp
+++ IHM - Alexandre Clarinard/mainwindow.cpp
@@ -22,7 +22,9 @@ MainWindow::MainWindow(QWidget *parent)
       _server(this)
 {
     ui->setupUi(this);
-    QPixmap pix("C:/Users/alexa/Pictures/BTS/Revue projet 1/toit immeuble.jpg");
+
+    /*IMAGE CAMERA DEFAUT*/
+    QPixmap pix("C:/Users/alexa/Pictures/image_recue.ppm"); //IMAGE PAR DEFAUT
     ui->label_image->setPixmap(pix.scaled(int(ui->label_image->width()),int(ui->label_image->height()),Qt::KeepAspectRatio));
 
     /*HORLOGE IHM*/
@@ -34,30 +36,29 @@ MainWindow::MainWindow(QWidget *parent)
     ui->widgetGraphe->addGraph();
     ui->widgetGraphe->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
     ui->widgetGraphe->graph(0)->setLineStyle(QCPGraph::lsNone);
-    // give the axes some labels:
+    // Donne aux axes des labels:
     ui->widgetGraphe->xAxis->setLabel("Heure");
     ui->widgetGraphe->yAxis->setLabel("Poids total de l'aire en kg");
-    // set axes ranges, so we see all data:
+    // Paramètre de la "portée visible" des axes:
     ui->widgetGraphe->xAxis->setRange(0, 24);
     ui->widgetGraphe->yAxis->setRange(0, 50);
     ui->widgetGraphe->replot();
 
     for(int i=0; i < 24 ; i++)
-        addPoint_constructeur(i,-1);    //reset du graphe
+        addPoint_constructeur(i,-1);
     chrono = 0;
 
-    /*SERVER TCP*/
+    /*SERVEUR TCP DONNEES AIR*/
     _server.listen(QHostAddress::Any, 4242);
     connect(&_server, SIGNAL(newConnection()), this, SLOT(onNewConnection()));
     qDebug() << "Serveur TCP données : " << _server.serverAddress().toString() << " au port : " << _server.serverPort();
-
 
     /*SERVEUR TCP IMAGE*/
     _server_image.listen(QHostAddress::Any, 4243);
     connect(&_server_image, SIGNAL(newConnection()), this, SLOT(onNewConnection_image()));
     qDebug() << "Serveur TCP image   : " << _server_image.serverAddress().toString() << " au port : " << _server_image.serverPort();
 
-    /*CONNECT BDD*/
+    /*TESTS CONNECT BDD*/
     bdd();
 
     /*LABEL IP*/
@@ -80,6 +81,7 @@ void MainWindow::UpdateTime()
 }
 
 /*GRAPHE IHM*/
+
 void MainWindow::addPoint_constructeur(double x, double y){
     qv_x.append(x);
     qv_y.append(y);
@@ -89,12 +91,6 @@ void MainWindow::addPoint(double x, double y)
 {
     qv_x.append(x);
     qv_y.replace(x,y);
-}
-
-void MainWindow::clearData()
-{
-    qv_x.clear();
-    qv_y.clear();
 }
 
 void MainWindow::plot()
@@ -143,7 +139,6 @@ void MainWindow::bdd()
             // plus explicite sur les causes de l'erreur
             qDebug() << requete.lastError();
         }
-        //dbMeteo.close(); // Fermeture de la base de données
     }
     else {
         qDebug() << "Echec d'ouverture de la base de donnée";
@@ -206,23 +201,23 @@ void MainWindow::onReadyRead()
 
     //données air
 
-    qDebug() << datas;
+    qDebug() << datas; //affiche la trame reçue
     QString string(datas);
     QStringList liste;
     liste << string.split(',');
     if((liste.at(0) == "$TRACQ") && (liste.at(8) == "*")){
-        ui->labelTempInfo->setText(liste.at(1) + " °C");
-        ui->labelVentInfo->setText(liste.at(2) + " km/h");
-        ui->labelHumideInfo->setText(liste.at(3) + " %");
-        ui->labelLumInfo->setText(liste.at(4) + " lux");
-        ui->labelAireInfo->setText(liste.at(5) + " kg");
+        ui->labelTempInfo->setText(liste.at(4) + " °C");
+        ui->labelVentInfo->setText(liste.at(1) + " km/h");
+        ui->labelHumideInfo->setText(liste.at(5) + " %");
+        ui->labelLumInfo->setText(liste.at(3) + " lux");
+        ui->labelAireInfo->setText(liste.at(2) + " kg");
 
-        this->temp  = liste.at(1).toFloat();
-        this->vit   = liste.at(2).toFloat();
-        this->hum   = liste.at(3).toFloat();
-        this->lum   = liste.at(4).toFloat();
-        this->poids = liste.at(5).toFloat();
-        qDebug() << temp << vit << hum << lum << poids;
+        this->vit  = liste.at(1).toFloat();
+        this->poids   = liste.at(2).toFloat();
+        this->lum   = liste.at(3).toFloat();
+        this->temp  = liste.at(4).toFloat();
+        this->hum = liste.at(5).toFloat();
+        qDebug() << vit << poids << lum << temp << hum;
 
         /// Gestion de la date + affichage (debug)
         QString stringDate = liste.at(6);
@@ -232,7 +227,6 @@ void MainWindow::onReadyRead()
 
         /*INSERTION DONNEES DANS LA BDD*/
         QSqlQuery requete;
-        //qDebug() <<
         requete.prepare("INSERT INTO mesures set temperature=:temp, vitesse_vent=:vit, humidite=:hum, luminosite=:lum, poids_aire=:poids");
         requete.bindValue(":temp",this->temp);
         requete.bindValue(":vit",this->vit);
@@ -242,16 +236,12 @@ void MainWindow::onReadyRead()
         requete.exec();
         qDebug() << requete.lastError().text();
 
-        /*datas.value(2).toFloat()->liste.at(1);
-        datas.value(3).toFloat()->liste.at(2);
-        datas.value(4).toFloat()->liste.at(3);
-        datas.value(5).toFloat()->liste.at(4);
-        datas.value(6).toFloat()->liste.at(5);*/
+            //
         addPoint(chrono, liste.at(5).toDouble());
 
         chrono = chrono + 1;
-        qDebug() << "+1 heure";
-        qDebug() << chrono;
+        qDebug() << "+1 heure"; //checkup changement d'heure
+        qDebug() << chrono; //affiche la nouvelle heure
         if (chrono >= 23)
         {
             chrono = 0;
@@ -275,11 +265,11 @@ void MainWindow::onReadyRead_image()
     cout << datas.size() << endl;
 
     if(check_image){
-        QFile::remove("F:/Documents/1. Cours/BTS SN IR 2/Informatique/Projet/Aire Rapace Github/projet-aire-rapace/Imageimage_recue.ppmimage_recue.ppm");
+        QFile::remove("C:/Users/alexa/Pictures/image_recue.ppm");   //enlève l'image précédente (si il y a)
         check_image=0;
     }
     //image caméra
-    QFile image("F:/Documents/1. Cours/BTS SN IR 2/Informatique/Projet/Aire Rapace Github/projet-aire-rapace/Imageimage_recue.ppm");
+    QFile image("C:/Users/alexa/Pictures/image_recue.ppm");
     image.open(QIODevice::ReadWrite | QIODevice::Append);
     image.write(datas);
     //image.close();
@@ -294,6 +284,6 @@ void MainWindow::onReadyRead_image()
 
 void MainWindow::on_pushButton_3_clicked()
 {
-    QFile::remove("F:/Documents/1. Cours/BTS SN IR 2/Informatique/Projet/Aire Rapace Github/projet-aire-rapace/Imageimage_recue.ppm");
+    QFile::remove("C:/Users/alexa/Pictures/image.png");
     close();
 }
--- IHM - Alexandre Clarinard/mainwindow.h
+++ IHM - Alexandre Clarinard/mainwindow.h
@@ -44,45 +44,84 @@ public:
      * @brief addPoint
      * @param x
      * @param y
+     * @details Permet de rajouter, ou de mettre à jour, les points dans le graphe.
      */
     void addPoint(double x, double y);
     /**
      * @brief addPoint_constructeur
      * @param x
      * @param y
+     * @details Permet de créer les points par défauts sur le graphe, en ordonnée négative, afin de pouvoir les mettre à jour.
      */
     void addPoint_constructeur(double x, double y);
     /**
-     * @brief clearData
-     */
-    void clearData();
-    /**
      * @brief plot
+     * @details Permet de mettre à jour un point dans le graphe.
      */
     void plot();
 
     /*TESTS CONNECTION IHM-SQL*/
+    /**
+     * @brief bdd
+     * @details Permet de se connecter à la Base De Données.
+     */
     void bdd();
 
 /*SERVEUR TCP*/
 public slots:
+    /**
+     * @brief onNewConnection
+     * @details S'exécute lorsqu'un client se connecte à la liaison TCP/IP, permet de recevoir les trames de données via liaison TCP/IP.
+     */
     void onNewConnection();
+    /**
+     * @brief onSocketStateChanged
+     * @param socketState
+     * @details S'exécute lorsqu'un client se déconnecte de la liaison TCP/IP, permet au programme de se supprimer la socket qui lui a été allouée.
+     */
     void onSocketStateChanged(QAbstractSocket::SocketState socketState);
+    /**
+     * @brief onReadyRead
+     * @details Permet d'extraire puis d'afficher sur l'IHM les données reçues via liaison TCP/IP. Envoie également les données extraites à la Base De Données.
+     */
     void onReadyRead();
 
+    /**
+     * @brief onNewConnection_image
+     * @details S'exécute lorsqu'un client se connecte à la liaison TCP/IP, permet de recevoir les images via liaison TCP/IP.
+     */
     void onNewConnection_image();
+    /**
+     * @brief onSocketStateChanged_image
+     * @param socketState
+     * @details S'exécute lorsqu'un client se déconnecte de la liaison TCP/IP, permet au programme de se supprimer la socket qui lui a été allouée.
+     */
     void onSocketStateChanged_image(QAbstractSocket::SocketState socketState);
+    /**
+     * @brief onReadyRead_image
+     * @details Permet d'extraire puis d'afficher sur l'IHM l'image reçue via liaison TCP/IP.
+     */
     void onReadyRead_image();
 
 private slots:
 
     /*HORLOGE IHM*/
-    void heure();
 
+    /**
+     * @brief UpdateTime
+     * @details Affiche l'heure en temps réel.
+     */
     void UpdateTime();
 
 
     /*GRAPHE IHM*/
+
+    /**void on_btn_clear_clicked(); //inutilisé**/
+
+    /**
+     * @brief on_pushButton_3_clicked
+     * @details Permet de quitter l'application
+     */
     void on_pushButton_3_clicked();
 
 private:
@@ -103,6 +142,7 @@ private:
     QList<QTcpSocket*>  _sockets;
     QList<QTcpSocket*>  _sockets_image;
 
+    /*TEST DE LA MORT*/
     QDate date;
     float temp,vit,hum,lum,poids;
     int chrono;
